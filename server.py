af8f1dba42b631e324900f92309c953ec1a7ba30
crispynuggets55 <emelinecromer@gmail.com>
2024-01-18 20:51:51 -0500

    changed color of upgraded pieces and working minimax prototype

--- server.py
+++ server.py
@@ -52,20 +52,24 @@ def moves(board, pturn):
                         continue
                     if '1' in piece or 'u' in piece:
                         if board[potential[0]][potential[1]] == '  ':
-                            board[tile[0]][tile[1]], board[potential[0]][potential[1]] = board[potential[0]][potential[1]], board[tile[0]][tile[1]]
+                            board[tile[0]][tile[1]], board[potential[0]][potential[1]] = board[potential[0]][
+                                potential[1]], board[tile[0]][tile[1]]
                             mooves.append(copy.deepcopy(board))
-                            board[tile[0]][tile[1]], board[potential[0]][potential[1]] = board[potential[0]][potential[1]], board[tile[0]][tile[1]]
+                            board[tile[0]][tile[1]], board[potential[0]][potential[1]] = board[potential[0]][
+                                potential[1]], board[tile[0]][tile[1]]
                         elif board[tile[0]][tile[1]][0] == board[potential[0]][potential[1]][0]:
                             continue
                         else:
                             nextp = (potential[0] + direction[0], potential[1] + direction[1])
                             if 0 <= nextp[0] <= len(board) - 1 and 0 <= nextp[1] <= len(board[row]) - 1:
                                 if board[nextp[0]][nextp[1]] == '  ':
-                                    board[nextp[0]][nextp[1]], board[tile[0]][tile[1]] = board[tile[0]][tile[1]], board[nextp[0]][nextp[1]]
+                                    board[nextp[0]][nextp[1]], board[tile[0]][tile[1]] = board[tile[0]][tile[1]], \
+                                        board[nextp[0]][nextp[1]]
                                     newboard = copy.deepcopy(board)
                                     newboard[potential[0]][potential[1]] = '  '
                                     mooves.append(newboard)
-                                    board[nextp[0]][nextp[1]], board[tile[0]][tile[1]] = board[tile[0]][tile[1]], board[nextp[0]][nextp[1]]
+                                    board[nextp[0]][nextp[1]], board[tile[0]][tile[1]] = board[tile[0]][tile[1]], \
+                                        board[nextp[0]][nextp[1]]
 
                 else:
                     if tile[0] == len(board) - 1:
@@ -84,26 +88,29 @@ def moves(board, pturn):
                             if 0 <= nextp[0] <= len(board) - 1 and 0 <= nextp[1] <= len(board[row]) - 1:
                                 if board[nextp[0]][nextp[1]] == '  ':
                                     board[nextp[0]][nextp[1]], board[tile[0]][tile[1]] = board[tile[0]][tile[1]], \
-                                    board[nextp[0]][nextp[1]]
+                                        board[nextp[0]][nextp[1]]
                                     newboard = copy.deepcopy(board)
                                     newboard[potential[0]][potential[1]] = '  '
                                     mooves.append(newboard)
                                     board[nextp[0]][nextp[1]], board[tile[0]][tile[1]] = board[tile[0]][tile[1]], \
-                                    board[nextp[0]][nextp[1]]
+                                        board[nextp[0]][nextp[1]]
 
     return mooves
 
 
-def minimax(board):
+def minimax(board, depth, maxdepth):
     score = winner(board)
 
-    if score != 0:
+    # check if the absolute value of the score is >= 100 or depth is greater than max depth if true return score, board
+    # otherwise continue with the line that says best= and the for loop
+
+    if depth > maxdepth or abs(score) >= 100:
         return score, board
 
     best = (1e9, None)
 
-    for move in moves(board):
-        score = maximin(move)
+    for move in moves(board, False):
+        score, b = maximin(move, depth=depth + 1, maxdepth=maxdepth)
 
         if score < best[0]:
             best = (score, move)
@@ -111,8 +118,21 @@ def minimax(board):
     return best
 
 
-def maximin(board):
-    return "hhhhhhhhhh"
+def maximin(board, depth, maxdepth):
+    score = winner(board)
+
+    if depth > maxdepth or abs(score) >= 100:
+        return score, board
+
+    best = (-1e9, None)
+
+    for move in moves(board, True):
+        score, b = minimax(move, depth=depth + 1, maxdepth=maxdepth)
+
+        if score > best[0]:
+            best = (score, move)
+
+    return best
 
 
 def winner(board):
@@ -123,15 +143,19 @@ def winner(board):
         for item in row:
             if "1" in item:
                 count1 += 1
+                if "u" in item:
+                    count1 += 1
             elif "2" in item:
                 count2 += 1
+                if "u" in item:
+                    count2 += 1
 
     if count1 == 0:
         return count2 * -100
     elif count2 == 0:
         return count1 * 100
 
-    return 0
+    return count1-count2
 
 
 @app.route("/")
@@ -147,7 +171,7 @@ def ai1():
     # print("\n"*5)
 
     # for move in moves(board, False):
-        # pprint.pprint(move)
+    # pprint.pprint(move)
 
     for row in range(len(board)):
         for column in range(len(board[row])):
@@ -165,7 +189,17 @@ def ai1():
                 except Exception:
                     pass
 
+    # todo try new minimax ai
+
     return {"board": board}
+
+
+@app.route("/ai2", methods=["POST"])
+def ai2():
+    body = request.get_json()
+    board = body["board"]
+    score, new_board = minimax(board, depth=0, maxdepth=3)
+    return {"board": new_board}
 
 
 if __name__ == "__main__":
